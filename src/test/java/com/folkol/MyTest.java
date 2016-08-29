526560a665906affa3c4e1d5354f03b4c2c8779a
Matte <mattias4@kth.se>
2016-08-29 12:59:02 +0200

    WIP

---
+++ src/test/java/com/folkol/MyTest.java
@@ -0,0 +1,11 @@
+package com.folkol;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MyTest {
+    @Test
+    public void test() throws Exception {
+        Assert.assertTrue(true);
+    }
+}
